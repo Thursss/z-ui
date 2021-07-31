fb691524a5d35408b0b65792d35d806d891a497e
DESKTOP-PGTMH7L\17205 <1720557879@qq.com>
2021-08-01 00:44:07 +0800

    Keep calm and commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>z-ui</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
